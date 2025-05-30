cee2a7cb47df220cc012b26717e7900235c3a336
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-05-30 18:23:08 +0200

    Fixed some more colour errors. Added breadcrumbs, not done though.

--- src/assets/pages/PrivacyPolicy.jsx
+++ src/assets/pages/PrivacyPolicy.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import '/src/css/pages/PrivacyPolicy.css'
 
 const PrivacyPolicy = () => {
     return (
-        <div>
-            <h1 className='h3'>Privacy Policy</h1>
-            <p>We have no interest of your data.</p>
-            <p>A cookie is created that communicates with the site, and only focus on essential usage.</p>
-            <p>Such as if you are on dark/light mode, which parts of the page you have visited.</p>
-            <p>We do not collect any personal information, and we do not share any data with third parties.</p>
-            <p>We respect your privacy and are committed to protecting your personal data.</p>
-            <p>By using our website, you consent to the collection and use of your information in accordance with this policy.</p>
-            <p>If you have any questions or concerns about our privacy policy, please contact us.</p>
-            <p>Last updated: May 30, 2025</p>
-        </div>
+            <section id="privacy-policy">
+                <h1 className='h3'>Privacy Policy</h1>
+                <div className='privacy-policy-content'>
+                    <p>We have no interest of your data.</p>
+                    <p>A cookie is created that communicates with the site, and only focus on essential usage.</p>
+                    <p>Such as if you are on dark/light mode, which parts of the page you have visited.</p>
+                    <p>We do not collect any personal information, and we do not share any data with third parties.</p>
+                    <p>We respect your privacy and are committed to protecting your personal data.</p>
+                    <p>By using our website, you consent to the collection and use of your information in accordance with this policy.</p>
+                    <p>If you have any questions or concerns about our privacy policy, please contact us.</p>
+                    <p>Last updated: May 30, 2025</p>
+                </div>
+            </section>
     )
 }
 
